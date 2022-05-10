be004af8b18c8563759d4bbf5a25a3a8c8c31852
Marc Andri Fuchs <marconymous@outlook.com>
2022-05-10 14:43:21 +0200

    Initial Commit V2

---
+++ src/main/java/dev/marconymous/bookshelf/model/Publisher.java
@@ -0,0 +1,9 @@
+package dev.marconymous.bookshelf.model;
+
+import java.util.List;
+
+public class Publisher {
+  private String publisherUUID;
+  private String publisher;
+  private List<Book> bookList;
+}
---
+++ src/main/java/dev/marconymous/bookshelf/service/Config.java
@@ -0,0 +1,8 @@
+package dev.marconymous.bookshelf.service;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/resource")
+public class Config extends Application {
+}
